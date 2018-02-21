496ebe27b5eb523759974e6a2596be3a4b7851b4
d.voyanets <diana.voyanets@gmail.com>
2018-02-21 23:33:17 +0300

    Done!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    var equationRoots = equation
+        .replace(/\s/g, '')
+        .split(/[*]x*[\^\d]*/)
+        .map((value) => parseInt(value));
+    
+    var [a, b, c] = [...equationRoots];
+
+    var D = Math.sqrt(b * b - 4 * a * c);
+    var x1 = (-b + D) * 1 / (2 * a);
+    var x2 = (-b - D) * 1 / (2 * a);
+    
+    return [x1, x2].map((value) => Math.round(value)).sort((a, b) => a - b);
 }
